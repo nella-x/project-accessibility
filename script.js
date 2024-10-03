f7906b203b8e8c276ff2219cd07b19a8da0248bd
Nella <nellax@me.com>
2024-10-03 12:20:29 +0200

    added alt to images

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const NEWS = [
   {
     img: 'assets/sitting.jpg',
+    alt: 'A drawing by Kafka',
     headline: `Kafka's Genius Unmatched by AI`,
     lead: `Kafka was a master of language, with a unique ability to create haunting and disturbing stories that explore the human condition. His work is characterized by its complexity, ambiguity, and psychological depth.`,
     body: `AI language models are not capable of writing as well as Franz Kafka. Kafka was a master of language, with a unique ability to create haunting and disturbing stories that explore the human condition. His work is characterized by its complexity, ambiguity, and psychological depth. AI language models are not able to replicate these qualities. 
@@ -9,6 +10,7 @@ const NEWS = [
   },
   {
     img: 'assets/glitter-head.jpg',
+    alt: 'Sculpture, Head of Franz Kafka',
     headline: `Kafka's Head, a Disco Ball Twist`,
     lead: `David Černý's iconic sculpture, Head of Franz Kafka, is a striking and controversial piece of art that has become a landmark in Prague, Czech Republic.`,
     body: `This towering, 10-meter-tall sculpture depicts the head of the renowned author, Franz Kafka, but with a unique twist: it's constructed from 42 independently rotating stainless steel panels, giving it a dazzling, disco ball-like appearance.
@@ -17,6 +19,7 @@ const NEWS = [
   },
   {
     img: 'assets/readers.jpeg',
+    alt: 'Two reading persons',
     headline: `Kafka's Complex Appeal: Who's Reading His Works?`,
     lead: `Kafka's works are often praised for their complex narratives, psychological depth, and literary style. These elements appeal to well-educated readers with a strong interest in literature and philosophy.`,
     body: `However, Kafka's writing can also be enjoyed by readers of various backgrounds and levels of literary experience. His stories often provoke thought and discussion, making them a popular choice for book clubs and literary circles.
@@ -25,6 +28,7 @@ const NEWS = [
   },
   {
     img: 'assets/museum-k.jpg',
+    alt: 'Outside of the Museum',
     headline: `A Journey into Kafka's World: The Kafka Museum in Prague`,
     lead: `Nestled in the heart of Prague, the Kafka Museum offers a captivating journey into the enigmatic world of Franz Kafka.`,
     body: `Housed within a historic building with a labyrinthine interior, the museum is a fitting tribute to the author's complex and thought-provoking works. <br> Visitors can delve into exhibits showcasing Kafka's life, writings, and the historical context that shaped his literary creations. Interactive displays, original manuscripts, and personal belongings bring his world to life, offering a unique and immersive experience for both dedicated Kafka fans and those seeking to explore Prague's rich cultural heritage.`,
@@ -32,6 +36,7 @@ const NEWS = [
   },
   {
     img: 'assets/der-prozess.jpg',
+    alt: 'Book cover from 1925',
     headline: `The Enduring Relevance of "The Trial"`,
     lead: `Franz Kafka's novel, "The Trial," continues to resonate with readers nearly a century after its publication. Its themes of alienation, bureaucracy, and the individual's struggle against an oppressive system remain strikingly relevant in today's world.`,
     body: `Kafka's portrayal of a protagonist who is arrested and charged with a crime he cannot understand mirrors the experiences of many individuals who feel powerless in the face of complex and often unjust systems. The novel's exploration of the dehumanizing effects of bureaucracy is particularly relevant in a world increasingly dominated by large corporations and government agencies.`,
@@ -50,7 +55,7 @@ const renderNews = (newsData) => {
   const bigNewsContainer = document.getElementById('bigNews');
   bigNewsContainer.innerHTML = `
     <div class="big-news">
-      <img class="big-img" src="${topNews.img}"> 
+      <img class="big-img" src="${topNews.img}" alt="${topNews.alt}"> 
     <article class="big-article">
       <h2>${topNews.headline}</h2>
         <p>${topNews.lead}</p>
@@ -69,7 +74,7 @@ const renderNews = (newsData) => {
   if (gridNews.length > 0) { // Check if there's any grid news before rendering
     smallNewsContainer.innerHTML = gridNews.map(news => `
     <div class="small-news">
-      <img class="small-img" src="${news.img}">
+      <img class="small-img" src="${news.img}" alt="${news.alt}">
       <article>
       <h3>${news.headline}</h3>
         <p>${news.lead}</p>
